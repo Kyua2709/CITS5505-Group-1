7b4b79fc4bc23d0f77960f5974be65310dd48914
Kexing Yuan <yuanfushuai@gmail.com>
2025-05-06 22:55:23 +0800

    Generate a secure SECRET_KEY

--- app/__init__.py
+++ app/__init__.py
@@ -9,16 +9,23 @@ def create_app():
         static_folder="static",  # 指向 app/static
         template_folder="templates"  # 指向 app/templates
     )
+    
+    # 配置 SECRET_KEY
+    app.config['SECRET_KEY'] = 'your_secret_key_here'  # 替换为一个随机的、唯一的字符串
+
+    # 配置数据库
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/uploads.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # 初始化数据库
     db.init_app(app)
 
+    # 创建数据库表
     with app.app_context():
-        db.create_all()  # 创建数据库表
+        db.create_all()
 
     # 注册路由蓝图
     from .routes import main_bp
     app.register_blueprint(main_bp)
 
-    return app
+    return app  
